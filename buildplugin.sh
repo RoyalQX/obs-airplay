cf03d63ec51626d84d587c8ca7085d3c7863bd24
natesway <64743334+natesway@users.noreply.github.com>
2022-11-03 13:38:55 -0400

    Create buildplugin.sh

---
+++ buildplugin.sh
@@ -0,0 +1,7 @@
+sudo apt-get install -y clang pkg-config libavcodec-dev libavformat-dev libavutil-dev libswresample-dev git libobs-dev libavahi-compat-libdnssd-dev libplist-dev libfdk-aac-dev
+git clone https://github.com/coddle-cpp/coddle.git && cd coddle && ./build.sh
+sudo ./deploy.sh
+cd ..
+git clone --recurse-submodules https://github.com/natesway/obs-airplay.git
+cd obs-airplay
+coddle
