7a8a4a0f91bb6b4dc67afd84c9ceb2d1575b5491
natesway <64743334+natesway@users.noreply.github.com>
2022-11-03 14:53:23 -0400

    Update buildplugin.sh

--- buildplugin.sh
+++ buildplugin.sh
@@ -1,4 +1,4 @@
-sudo apt-get install -y clang pkg-config libavcodec-dev libavformat-dev libavutil-dev libswresample-dev git libobs-dev libavahi-compat-libdnssd-dev libplist-dev libfdk-aac-dev
+sudo apt-get install -y clang pkg-config libavcodec-dev libavformat-dev libavutil-dev libswresample-dev git libobs-dev libavahi-compat-libdnssd-dev libplist-dev libswscale-dev libfdk-aac-dev
 git clone https://github.com/coddle-cpp/coddle.git && cd coddle && ./build.sh
 sudo ./deploy.sh
 cd ..
